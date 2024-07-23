911194f238eba02764fab6c2afef6e20a7918ac6
Cem <cemcakmak159@gmail.com>
2024-07-23 16:54:10 +0300

    dataset
    a dataset has been added

--- regression.py
+++ regression.py
@@ -2,6 +2,10 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import openpyxl
 
-workbook = openpyxl.Workbook()
-sheet = workbook.active
+dataset = openpyxl.load_workbook("database/dataset.xlsx")
 
+for row in range(0, dataset.max_row):
+    for col in dataframe1.iter_cols(1, dataset.max_column):
+        X[col][row] = col[row].value
+
+print(X)
