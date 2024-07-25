fdf5f28a4bde464e53ae2374a2550a55e1a43adc
Cem <cemcakmak159@gmail.com>
2024-07-25 15:44:18 +0300

    getting data
    getting data from 2 seperate matrices for both independant and dependant data.

--- regression.py
+++ regression.py
@@ -8,27 +8,37 @@ def read_excel_to_matrix(file_path):
     sheet = workbook.active
     
     #Initialize an empty matrix
-    data_matrix = []
+    matrix_X = []
+    matrix_Y = []
 
     #Iterate through the rows in the sheet
-    for X in range(2, sheet.max_row + 1):  # Assuming 1-based index
+    for X in range(2, sheet.max_row + 1):
         row_data = []
-        for col in range(2, sheet.max_column + 1):  # Assuming 1-based index
+        for col in range(2, sheet.max_column):
             cell_value = sheet.cell(row=X, column=col).value
             row_data.append(cell_value)
-        data_matrix.append(row_data)
+        matrix_X.append(row_data)
     
+    for Y in range(2, sheet.max_row + 1):
+        row_data = []
+        cell_value = sheet.cell(row=Y, column=7).value
+        row_data.append(cell_value)
+        matrix_Y.append(row_data)
+
     #Close the workbook
     workbook.close()
     
-    return data_matrix
+    return matrix_X, matrix_Y
 
 #Define the file path
 file_path = 'dataset.xlsx'
 
 #Read the data into a matrix
-matrix = read_excel_to_matrix(file_path)
+matrix_X, matrix_Y = read_excel_to_matrix(file_path)
 
 #Print the matrix
-for X in matrix:
+for X in matrix_X:
     print(X)
+
+for Y in matrix_Y:
+    print(Y)
