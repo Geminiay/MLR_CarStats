6b49e562d1cb716d5c94534daac2668ac18c90da
Cem <cemcakmak159@gmail.com>
2024-07-26 15:13:05 +0300

    matrices
    change the approach of forming matrices into more proper way

--- regression.py
+++ regression.py
@@ -8,8 +8,8 @@ def read_excel_to_matrix(file_path):
     sheet = workbook.active
     
     #Initialize an empty matrix
-    matrix_X = []
-    matrix_Y = []
+    data_X = []
+    data_y = []
 
     #Iterate through the rows in the sheet
     for X in range(2, sheet.max_row + 1):
@@ -17,28 +17,29 @@ def read_excel_to_matrix(file_path):
         for col in range(2, sheet.max_column):
             cell_value = sheet.cell(row=X, column=col).value
             row_data.append(cell_value)
-        matrix_X.append(row_data)
+        data_X.append(row_data)
     
-    for Y in range(2, sheet.max_row + 1):
-        row_data = []
-        cell_value = sheet.cell(row=Y, column=7).value
-        row_data.append(cell_value)
-        matrix_Y.append(row_data)
+    for y in range(2, sheet.max_row + 1):
+        cell_value = sheet.cell(row=y, column=7).value
+        data_y.append(cell_value)
+
+    array_X = np.array(data_X)
+    array_y = np.array(data_y)
 
     #Close the workbook
     workbook.close()
     
-    return matrix_X, matrix_Y
+    return array_X, array_y
 
 #Define the file path
 file_path = 'dataset.xlsx'
 
 #Read the data into a matrix
-matrix_X, matrix_Y = read_excel_to_matrix(file_path)
+X , y = read_excel_to_matrix(file_path)
 
-#Print the matrix
-for X in matrix_X:
-    print(X)
 
-for Y in matrix_Y:
-    print(Y)
+print("Feature Matrix X:\n")
+print(X)
+
+print("Target Array y:")
+print(y)
